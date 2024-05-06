9f540d933c00494788091940c126ebefce7fe50b
William Ndirangu <wwagura0@gmail.com>
2024-05-06 17:18:14 +0300

    Creates Components

---
+++ src/components/GameCard.js
@@ -0,0 +1,34 @@
+import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
+import { Game } from '../hooks/useGames';
+import PlatformIconList from './PlatformIconList.js';
+import CriticScore from './CriticScore';
+import getCroppedImageUrl from '../services/image-url';
+import Emoji from './Emoji';
+
+interface Props {
+  game: Game;
+}
+const GameCard = ({ game }: Props) => {
+  return (
+    <>
+      <Card>
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms?.map((p) => p.platform)}
+            />
+
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize="2xl">
+            {game.name}
+            <Emoji rating={game.rating_top} />
+          </Heading>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+
+export default GameCard;

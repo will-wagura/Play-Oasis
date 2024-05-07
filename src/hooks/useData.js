52253aafbba0c1a5c16a3bf40fb2a62f6ea9a235
William Ndirangu <wwagura0@gmail.com>
2024-05-08 00:20:16 +0300

    Adds babel to dev dependencies and fixes error

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -24,7 +24,7 @@ const useData = (endpoint, requestConfig, deps) => {
       });
 
     return () => controller.abort();
-  }, deps || []);
+  }, [endpoint, requestConfig]);
 
   return { data, error, isLoading };
 };

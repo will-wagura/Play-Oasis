cedf4145ec2298080f62e22010320265cd7874a3
William Ndirangu <wwagura0@gmail.com>
2024-05-08 01:18:53 +0300

    Fix error

--- src/services/api-clients.js
+++ src/services/api-clients.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api/",
   params: {
-    key: import.meta.env.REACT_APP_RAWG_API_KEY,
+    key: process.env.REACT_APP_RAWG_API_KEY,
   },
 });
